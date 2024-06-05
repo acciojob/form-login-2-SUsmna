ac33e5b51e86c9be6694098cd5929ad98ea5329e
SUsmna <159513831+SUsmna@users.noreply.github.com>
2024-06-05 11:45:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.getElementById('loginForm').addEventListener('submit',function(event){
+	event.preventDefault();
+	const firstName = document.getElementById('firstName').value;
+
+	const lastname = document.getElementById('lastName').value;
+
+	const phoneNumber = document.getElementById('phoneNumber').value;
+
+	const email = document.getElementById('email').value;
+
+	alert(`First Name: ${firstName} Last Name: ${lastname} Phone Number: ${phoneNumber} Email ID: ${email}`);
+});
